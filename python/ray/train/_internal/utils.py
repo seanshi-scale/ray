018a419ea95496d0fa9e5a1044360f60019efd7d
Sean Shi <sean.shi@scale.com>
2024-07-17 12:13:08 -0700

    put in session dir

--- python/ray/train/_internal/utils.py
+++ python/ray/train/_internal/utils.py
@@ -22,6 +22,9 @@ from ray.air._internal.util import StartTraceback, find_free_port
 from ray.exceptions import RayActorError
 from ray.types import ObjectRef
 
+# Terrible code importing
+from ray._private.worker import _global_node
+
 T = TypeVar("T")
 
 logger = logging.getLogger(__name__)
@@ -64,7 +67,7 @@ def check_for_failure(
 
 def get_address_and_port() -> Tuple[str, int]:
     """Returns the IP address and a free port on this node."""
-    addr = ray.util.get_cached_node_ip_address()  # TODO session_dir
+    addr = ray.util.get_cached_node_ip_address(_global_node._session_dir)  # TODO this code is quite hacky
     port = find_free_port()
 
     return addr, port
